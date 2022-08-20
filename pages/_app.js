14206e9f5aa6c49315962ed05a00b87089945401
Sam Bradbury <sam.bradbury@hotmail.com>
2022-08-20 13:44:23 +1000

    added a fact api call

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 // import "../styles/globals.css";
 import "../styles/style.css";
 import Link from "next/link";
+import Facts from "../lib/components/fact";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="wrapper">
       <header className="header">
         <h2>Lucky Puppy</h2>
+        <Facts />
       </header>
       <aside className="display-border aside aside-1">
         <div>
@@ -16,6 +18,9 @@ function MyApp({ Component, pageProps }) {
             </Link>
             <Link href="/about">
               <a>About</a>
+            </Link>
+            <Link href="/vet">
+              <a>Vet Records</a>
             </Link>
             <Link href="/gallery">
               <a>Gallery</a>
---
+++ pages/vet.js
@@ -0,0 +1,7 @@
+export default function Vet() {
+  return (
+    <div className="main">
+      <h1>Vet</h1>
+    </div>
+  );
+}
