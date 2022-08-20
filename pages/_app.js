0c7b73ff12989a7f090272f231820fc59e569263
Sam Bradbury <sam.bradbury@hotmail.com>
2022-08-20 12:16:51 +1000

    changed styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,34 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
+import "../styles/style.css";
 import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="container">
-      <nav className="nav">
+    <div className="wrapper">
+      <header className="header">
         <h2>Lucky Puppy</h2>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/gallery">
-          <a>Gallery</a>
-        </Link>
-        <Link href="/contact">
-          <a>Contact</a>
-        </Link>
-      </nav>
+      </header>
+      <aside class="display-border aside aside-1">
+        <div>
+          <nav>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+            <Link href="/gallery">
+              <a>Gallery</a>
+            </Link>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </nav>
+        </div>
+      </aside>
       <Component {...pageProps} />
+
+      <footer class="display-border footer">Footer</footer>
     </div>
   );
 }
