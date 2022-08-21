b08481c9101d7f5399ee737609271431efa9a622
Sam Bradbury <sam.bradbury@hotmail.com>
2022-08-21 22:54:40 +1000

    getting records working

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,8 +1,3 @@
-import { useState, useEffect } from "react";
-import { AdvancedImage } from "@cloudinary/react";
-import { Cloudinary } from "@cloudinary/url-gen";
-import { fill } from "@cloudinary/url-gen/actions/resize";
-
 export default function Gallery() {
   return (
     <section className="main">
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { MakeRecord, GetLatestRecord } from "../lib/components/NoteOperations";
+
 export default function Home() {
   return (
     <section className="main">
       <h1>Lucky The Puppy</h1>
+      <MakeRecord />
+
+      <GetLatestRecord />
       <h1>Latest pics</h1>
       <h1>Recent Post</h1>
     </section>
