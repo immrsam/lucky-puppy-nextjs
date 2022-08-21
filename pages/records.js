16b85fcdc2e682b9ca7f6dc3f4b8c987277660b8
Sam Bradbury <sam.bradbury@hotmail.com>
2022-08-21 21:34:53 +1000

    records combined into a single file and fixed key prop issue

--- pages/records.js
+++ pages/records.js
@@ -1,11 +1,11 @@
-import GetRecords from "../lib/components/getRecords";
-import NoteOperations from "../lib/components/NoteOperations";
+// import GetRecords from "../lib/components/getRecords";
+import { MakeRecord, GetRecords } from "../lib/components/NoteOperations";
 
 export default function Records() {
   return (
     <div className="main">
       <h1>Records</h1>
-      <NoteOperations />
+      <MakeRecord />
       <GetRecords />
     </div>
   );
