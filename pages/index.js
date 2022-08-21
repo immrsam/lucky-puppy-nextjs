f2a3e68ba05d7603a0d5d72861a9ac6a502be9f8
Sam Bradbury <sam.bradbury@hotmail.com>
2022-08-21 12:52:14 +1000

    white box for main content

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
 export default function Home() {
   return (
     <section className="main">
-      <h1>Lucky Puppy</h1>
-      <p>
-        Ipsum non mollit pariatur elit id proident magna adipisicing
-        reprehenderit voluptate quis. Deserunt veniam eiusmod dolor quis aute
-        qui esse quis Lorem. Fugiat sint laboris excepteur nulla esse labore
-        irure adipisicing. Commodo mollit mollit laborum pariatur ipsum non
-        incididunt cupidatat ipsum duis consectetur qui ea Lorem. Eu dolor
-        incididunt voluptate cillum veniam labore. Sunt dolore quis cupidatat
-        reprehenderit esse voluptate quis proident laborum mollit tempor ea
-        sunt. Laboris excepteur adipisicing quis id cillum. Cupidatat pariatur
-        ea duis consequat laborum aliquip do dolore ullamco adipisicing. Aliqua
-        ex cillum incididunt in laborum magna minim aliqua do adipisicing irure
-        tempor. Ut tempor dolore eu occaecat. Voluptate consectetur do pariatur
-        adipisicing consequat consectetur incididunt.
-      </p>
+      <h1>Lucky The Puppy</h1>
+      <h1>Latest pics</h1>
+      <h1>Recent Post</h1>
     </section>
   );
 }
