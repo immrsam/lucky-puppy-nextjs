8e82c3980e023d25f987e04e1f7eac3f05255e7c
Sam Bradbury <sam.bradbury@hotmail.com>
2022-08-21 19:09:27 +1000

    record keeping

--- pages/records.js
+++ pages/records.js
@@ -1,7 +1,12 @@
+import GetRecords from "../lib/components/getRecords";
+import NoteOperations from "../lib/components/NoteOperations";
+
 export default function Records() {
   return (
     <div className="main">
       <h1>Records</h1>
+      <NoteOperations />
+      {/* <GetRecords /> */}
     </div>
   );
 }
